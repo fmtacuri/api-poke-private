e93cd553bc88afc1014f2e0c18c798f3becee15a
Freddy Tacuri <ftacurip@pichincha.com>
2022-11-12 11:27:17 -0500

    add image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,17 @@ import {Component} from '@angular/core';
 import {PokemonService} from './app.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Maestría en software';
-  pokemons:any;
-  constructor( private pokemon: PokemonService){
+  pokemons: any;
+
+  constructor(private pokemon: PokemonService) {
   }
 
   async ngOnInit(): Promise<any> {
     this.pokemons = await this.pokemon.getPokemon();
-    console.log(this.pokemons.results)
+    this.pokemons = this.pokemons.results;
   }
 }
